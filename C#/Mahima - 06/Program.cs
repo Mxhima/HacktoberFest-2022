4765a0e73b59b248120e8fb945b58fbca3da53f1
Mahima Hemachandra <nomad.inof@gmail.com>
2022-10-11 10:15:12 +0530

    Answer for issue 06

---
+++ C#/Mahima - 06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        int sum = 0;
+        foreach (int item in array)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
